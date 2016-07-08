7a7c852c5bbcbba28ec7bccc11d94c9e57e4ba1c
Silas Rech <lenovouser@minora.io>
2016-07-08 15:00:18 +0200

    Add some more tests

--- test/mongodb.js
+++ test/mongodb.js
@@ -44,3 +44,41 @@ test('should not connect to the database', t => {
     t.pass(error.toString());
   });
 });
+
+test('should insert a document into the database', t => {
+  const url = `mongodb://${cfg.good.user}:${cfg.good.auth}@${cfg.good.host}:${cfg.good.port}/${cfg.good.db}`;
+  const db = new mongodb(url, cfg.good.db, true);
+  return db.insert({ test: 'test' })
+  .then(() => {
+    t.pass('Inserted document into the database');
+  })
+  .catch((error) => {
+    t.pass(error.toString());
+  });
+});
+
+test('should find documents in the database', t => {
+  const url = `mongodb://${cfg.good.user}:${cfg.good.auth}@${cfg.good.host}:${cfg.good.port}/${cfg.good.db}`;
+  const db = new mongodb(url, cfg.good.db, true);
+  return db.find()
+  .then(() => {
+    t.pass('Found documents');
+  })
+  .catch((error) => {
+    t.fail(error.toString());
+  });
+});
+
+test('should remove a document in the database', t => {
+  const url = `mongodb://${cfg.good.user}:${cfg.good.auth}@${cfg.good.host}:${cfg.good.port}/${cfg.good.db}`;
+  const db = new mongodb(url, cfg.good.db, true);
+  return db.insert({ test: 'test' }).then(() => {
+    db.remove({ test: 'test' })
+    .then(() => {
+      t.pass('Removed documents');
+    })
+    .catch((error) => {
+      t.fail(error.toString());
+    });
+  });
+});
